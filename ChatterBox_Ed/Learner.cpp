15bf7e4e01a10129cf8883ba28f06debcb2efc32
nagie <nagie.tk@gmail.com>
2020-07-31 14:15:25 -0700

    base files of ChatterBox_Ed

---
+++ ChatterBox_Ed/Learner.cpp
@@ -0,0 +1,12 @@
+#include "Learner.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*
+	The following function will look for the passed phrase in the memory file. 
+	If there is a match, the accompany reponse will be stored below the initial phrase,
+	will be outputed
+
+*/
